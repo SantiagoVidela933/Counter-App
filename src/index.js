95f44fd8cd878e640c392b52509929ba95edb174
SantiagoVidela933 <santi.videla.2@gmail.com>
2022-06-09 11:24:33 -0300

    commit de prueba

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-// import PrimeraApp from './PrimeraApp';
+// import PrimeraApp from './PrimeraApp'; antes
 import CounterApp from './CounterApp'
 import './index.css'
 
