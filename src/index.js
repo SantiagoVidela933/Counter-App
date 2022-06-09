9e23c28cc2402b2501d3498f4f1cda7fa5028973
SantiagoVidela933 <santi.videla.2@gmail.com>
2022-06-09 11:08:23 -0300

    tareaCounterApp

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-import PrimeraApp from './PrimeraApp';
+// import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <PrimeraApp />
+    <CounterApp  value={10}/>
   </React.StrictMode>
 );
