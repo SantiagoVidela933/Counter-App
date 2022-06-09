9f19ef0a2449a32d53326f61aeb42095413e012d
SantiagoVidela933 <santi.videla.2@gmail.com>
2022-06-09 12:24:34 -0300

    Counter App Finish

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
-const CounterApp = ( { value } ) => {
+const CounterApp = ( { value=10 } ) => {
+
+    const [ counter, setCounter ] = useState(value);
+
+    //handleAdd
+    const handleAddSuma = () =>{setCounter(counter+1);}
+    const handleReset = () =>{setCounter(value);}
+    const handleResta = () =>{setCounter(counter-1);}
+
   return (
     <>
         <h1>CounterApp</h1>
-        <h2> {  value } </h2>
+        <h2> { counter } </h2>
+
+        <button onClick={handleAddSuma}>+1</button>
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={handleResta}>-1</button>
+
     </>
   )
 }
